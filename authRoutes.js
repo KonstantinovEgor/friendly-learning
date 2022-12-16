cec4c1ee5dc241c60fbdb514f07cab3f319ea832
rodnikovlad <96655500+rodnikovlad@users.noreply.github.com>
2022-12-17 00:45:18 +0300

    added autorization route and user search

--- authRoutes.js
+++ authRoutes.js
@@ -4,13 +4,32 @@ class authRoutes {
     registration = (req, res) => {
         const body = req.body;
         bd.createUser(body);
-        console.log(req.body.name);
         res.json('reg router');
     };
 
-    login = (req, res) => {
-        res.json('login router');
+    login = async (req, res) => {
+        const body = req.body;
+
+        if(await bd.login(body)) res.json([{'status' : 'ok'}, {'access_token' : 'string'}, {'refresh_token' : 'string'}]);
+        else res.json([{'status' : 'error'}, {'message' : 'Ошибка при выполнении запроса'}]);
+
     };
+<<<<<<< Updated upstream
+=======
+
+    users = async (req, res) => {
+        const body = req.body;
+        let arr = await bd.getAllUsers(body);
+        res.json(arr);
+    }
+
+    user = async (req, res) => {
+        const body = req.body;
+        let arr = await bd.getUser(body, req.query.id);
+        console.log(arr);
+        res.json(arr);
+    }
+>>>>>>> Stashed changes
 }
 
 module.exports = new authRoutes();
--- bd.js
+++ bd.js
@@ -20,6 +20,76 @@ class bd {
             console.log(error);
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    getAllUsers = async(obj) => {
+        try{
+            const array = (await fs.readFileSync(this.pathToUsersModel, () => {})).toString().split("\n");
+
+            var arrayObj = [];
+            for(let i = 0; i < array.length - 1; i++){
+                var temp = array[i].split(':');
+                let arr = {
+                    id: parseInt(temp[0]),
+                    login: temp[1],
+                    password: temp[2]
+                }
+                arrayObj[i] = arr;
+            } 
+
+            return arrayObj;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    getUser = async(obj, id) => {
+        try{
+            const array = (await fs.readFileSync(this.pathToUsersModel, () => {})).toString().split("\n");
+
+            var arrayObj = [];
+            for(let i = 0; i < array.length - 1; i++){
+                var temp = array[i].split(':');
+                if(temp[0] === id){
+                    let arr = {
+                        id: parseInt(temp[0]),
+                        login: temp[1],
+                        password: temp[2]
+                    }
+
+                    arrayObj = arr;
+                }
+            } 
+            return arrayObj;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    login = async(obj) => {
+        try{
+            console.log(obj.login);
+            console.log(obj.password);
+            const array = (await fs.readFileSync(this.pathToUsersModel, () => {})).toString().split("\n");
+
+            let userTemp =  false;
+            for(let i = 0; i < array.length - 1; i++){
+                let temp = array[i].split(':');
+
+                if(obj.login.toString() === temp[1] && obj.password.toString() === temp[2] && userTemp === false) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+>>>>>>> Stashed changes
 }
 
 module.exports = new bd();
