1040489e44ac621afeda00bbecc9b045408a1a43
rodnikovlad <96655500+rodnikovlad@users.noreply.github.com>
2022-12-18 15:24:46 +0300

    rodnikovlad
    TL-00007 Хэширование пароля

--- bd.js
+++ bd.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const fs = require('fs');
 
+const privateService = require('./privateService');
+
 class bd {
     pathToUsersModel = path.join(__dirname, 'models', 'users.txt');
 
-    createUser = (obj) => {
+    createUser = async (obj) => {
         try {
             const id = 1;
             const login = obj.login;
             const password = obj.password;
-            const stringToSave = `${id} ${login} ${password}\n`;
+            const hashPassword = await privateService.cryptPassword(password);
+            const stringToSave = `${id} ${login} ${hashPassword}\n`;
 
             fs.appendFileSync(this.pathToUsersModel, stringToSave);
         } catch(error) {
