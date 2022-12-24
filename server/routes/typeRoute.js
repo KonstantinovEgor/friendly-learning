09132c3eeffdb7e40c5705c443eb4e3d4e7b82c1
Konstantinov <egaega802@gmail.com>
2022-12-24 20:51:22 +0300

    e.konstantinov
    update

--- server/routes/typeRoute.js
+++ server/routes/typeRoute.js
@@ -4,6 +4,7 @@ const router = new Router();
 const typeController = require('../controllers/typeController');
 
 router.post('/create', typeController.create);
+router.post('/delete', typeController.delete);
 router.get('/get', typeController.get);
 
 module.exports = router;
